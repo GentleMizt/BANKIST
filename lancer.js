636ac77df65d087f07f5a9010885ad14a20fe077
GentleMizt <olamideiyanda18@gmail.com>
2023-06-28 15:06:07 +0100

    progress

--- lancer.js
+++ lancer.js
@@ -174,3 +174,6 @@ currenciesUnique.forEach((value, _, map) => {
 });
 
 // It is important to note that a set doesn't have keys in them, so in a forEach Method, there is no index to assign a value to. A set just makes use of the values.
+
+// DOM MANIPULATION OF THE BANKIST APP
+
