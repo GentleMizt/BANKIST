a8bc30e11386b57b044129df0ee00a8ea64ffa9e
GentleMizt <olamideiyanda18@gmail.com>
2023-06-28 15:07:37 +0100

    progress

--- lancer.js
+++ lancer.js
@@ -61,6 +61,8 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+
+// DOM MANIPULATION OF THE BANKIST APP
 const displayMovements = function (movements){
   
 }
