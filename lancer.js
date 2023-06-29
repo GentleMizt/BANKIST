fd0e96feaf82ea3269704269d47fac66e48de461
GentleMizt <olamideiyanda18@gmail.com>
2023-06-29 06:37:15 +0100

    progress

--- lancer.js
+++ lancer.js
@@ -237,3 +237,9 @@ const checkData = function (dogsJulia, dogsKate) {
 
 // MAP METHOD IN PRACTICE
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const eurToUsd = 1.1;
+const movementToUsd = movements.map((mov)=>{
+  return mov * eurToUsd;
+})
+
