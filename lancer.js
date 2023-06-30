0b1bcb023d63abab2dbdc7605b7c562d75331cf4
GentleMizt <olamideiyanda18@gmail.com>
2023-06-30 08:00:05 +0100

    progress

--- lancer.js
+++ lancer.js
@@ -295,3 +295,8 @@ const balance = movements.reduce((acc, cur, i, arr)=>{
 }, 0); 
 
 console.log(balance);
+
+let balance2 = 0;
+for (const mov of movements) balance2 += mov;
+console.log(balance2);
+
