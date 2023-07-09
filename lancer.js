febb7d7fe643f6805385c5a925e3a43b4e6596df
GentleMizt <olamideiyanda18@gmail.com>
2023-07-09 22:32:27 +0100

    progress

--- lancer.js
+++ lancer.js
@@ -88,7 +88,9 @@ calcDisplayBalance(account1.movements);
 
 const calcDisplaySummary = function(movements){
   const incomes = movements.filter(mov => mov > 0).reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`
+  labelSumIn.textContent = `${incomes}€`;
+  const outgoing = movements.filter(mov => mov < 0).reduce((acc, mov) => acc + mov, 0);
+  
 }
 calcDisplaySummary(account1.movements);
 
