b4146973b32ccee5e9683f81f6fe0c97e5e4ccdc
GentleMizt <olamideiyanda18@gmail.com>
2023-07-14 22:15:17 +0100

    progress

--- lancer.js
+++ lancer.js
@@ -563,4 +563,7 @@ const arr = [[1,2,3], [4,5,6], 7,8];
 console.log(arr.flat()); // It does not take in any call backs.
 
 const arrDeep = [[[1,2],3,], [4, [5,6]], 7,8];
-console.log(arrDeep.flat(2));
+console.log(arrDeep.flat(2)); // goes in 2 levels deep and flatens the array.
+
+const accountMovements = accounts.map(acc => acc.movements)
+log
