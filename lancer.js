f011fed57af2654cdec8707311c3bf8f94995e3b
Mapelujo <olamideiyanda18@gmail.com>
2023-09-11 17:55:27 +0100

    progress

--- lancer.js
+++ lancer.js
@@ -257,4 +257,25 @@ btnSort.addEventListener('click', e => {
 console.log(23 === 23.0);
 console.log(0.1 + 0.2);
 
+// Converting strings to numbers
 console.log(+'23');
+
+// Parsing strings into numbers
+console.log(Number.parseInt('30px', 10));
+console.log(Number.parseInt('2.5rem', 10));
+
+console.log(Number.parseFloat('2.5rem'));
+
+// chcking if value is NaN
+console.log(Number.isNaN(20));
+console.log(Number.isNaN(+'20x'));
+console.log(Number.isNaN(23/0));
+
+// Checking if value is a number
+console.log(Number.isFinite(20));
+console.log(Number.isFinite('20'));
+console.log(Number.isFinite(+'20x'));
+console.log(Number.isFinite(23 / 0));
+
+console.log(Number.isInteger(24.0));
+console.log(Number.isInteger(+'23'));
