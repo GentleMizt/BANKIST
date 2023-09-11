e18baf6c550570251595daedbd89948abd011f62
Mapelujo <olamideiyanda18@gmail.com>
2023-09-11 20:29:14 +0100

    progress

--- lancer.js
+++ lancer.js
@@ -307,22 +307,39 @@ btnSort.addEventListener('click', e => {
 // THE REMAINDER OPERATOR
 // - Simply returns the remainder of a division.
 
-console.log(5 % 2);
-console.log(8 % 3);
+// console.log(5 % 2);
+// console.log(8 % 3);
 
-console.log(6 % 2);
+// console.log(6 % 2);
 
-const isEven = n => n % 2 === 0;
-console.log(isEven(3));
-console.log(isEven(4));
+// const isEven = n => n % 2 === 0;
+// console.log(isEven(3));
+// console.log(isEven(4));
 
-labelBalance.addEventListener('click', () => {
-  [...document.querySelectorAll('.movements__row')].forEach((row, i) => {
-    if (i % 2 === 0) {
-      row.style.backgroundColor = 'orangered';
-    }
-    if (i % 3 === 0) {
-      row.style.backgroundColor = 'blue'
-    }
-  });
-});
+
+// labelBalance.addEventListener('click', () => {
+//   [...document.querySelectorAll('.movements__row')].forEach((row, i) => {
+//     if (i % 2 === 0) {
+//       row.style.backgroundColor = 'orangered';
+//     }
+//     if (i % 3 === 0) {
+//       row.style.backgroundColor = 'blue'
+//     }
+//   });
+// });
+
+
+//// NUMERICAL OPERATORS //////////////
+
+const diameter = 287_460_000_000;
+console.log(diameter);
+
+const price = 345_99;
+console.log(price);
+
+const transferFee = 15_00;
+
+const PI = 3.1415;
+console.log(PI);
+
+console.log(Number('23_000'));
