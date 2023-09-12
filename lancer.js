c3f9af6d01e1133fa2eabab1121931647c8d9c94
Mapelujo <olamideiyanda18@gmail.com>
2023-09-12 13:41:20 +0100

    progress

--- lancer.js
+++ lancer.js
@@ -316,7 +316,6 @@ btnSort.addEventListener('click', e => {
 // console.log(isEven(3));
 // console.log(isEven(4));
 
-
 // labelBalance.addEventListener('click', () => {
 //   [...document.querySelectorAll('.movements__row')].forEach((row, i) => {
 //     if (i % 2 === 0) {
@@ -327,7 +326,6 @@ btnSort.addEventListener('click', e => {
 //     }
 //   });
 // });
-
 
 //// NUMERICAL OPERATORS //////////////
 
@@ -343,8 +341,6 @@ btnSort.addEventListener('click', e => {
 // console.log(PI);
 
 // console.log(Number('23_000'));
-
-
 
 ////////// BIGINT /////////////
 // console.log(2 ** 53-1);
@@ -374,5 +370,24 @@ btnSort.addEventListener('click', e => {
 // console.log(15n/3n);
 // console.log(10/3);
 
+/////////// CREATING DATES //////////////
 
-// 
+// Create a Date. (There are four ways of creating a date.)\
+
+// const now = new Date(); // 1.
+// console.log(now);
+
+// console.log(new Date('Sep 12 2023 12:52:04 ')); // 2.
+
+// console.log(new Date('April 28, 2004'));
+
+// console.log(new Date(account1.movementsDates[0]));
+
+// console.log(new Date(2037, 3, 28, 1, 23, 5)); // 3.
+
+// console.log(new Date(0));
+// console.log(new Date(3 * 24 * 60 * 60 * 1000));
+
+// Working with Dates
+const future = new Date(2037, 3, 28, 1, 23);
+console.log(future);
