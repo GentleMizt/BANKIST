3aca8ee0d286f9da048bc9a713e3732b077d178e
Mapelujo <olamideiyanda18@gmail.com>
2023-09-13 22:30:56 +0100

    progress

--- lancer.js
+++ lancer.js
@@ -485,3 +485,17 @@ btnSort.addEventListener('click', e => {
 
 // INTERNALIZING DATES //
 
+// const num = 43238843.23;
+
+// const options = {
+//   style: 'currency',
+//   currency: 'USD',
+//   unit: 'mile-per-hour',
+//   useGrouping: true,
+// }
+
+// console.log('US:',new Intl.NumberFormat('en-US', options).format(num));
+// console.log('Germany:',new Intl.NumberFormat('de-DE', options).format(num));
+// console.log('Syria:',new Intl.NumberFormat('ar-SY', options).format(num));
+// console.log('Navigator Language:',new Intl.NumberFormat(navigator.language, options).format(num));
+
