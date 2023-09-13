99014d7d7bf923ee0d05759ec2dd083bbf3b42a5
Mapelujo <olamideiyanda18@gmail.com>
2023-09-13 22:07:12 +0100

    progress

--- lancer.js
+++ lancer.js
@@ -18,9 +18,9 @@ const account1 = {
     '2020-01-28T09:15:04.904Z',
     '2020-04-01T10:17:24.185Z',
     '2020-05-08T14:11:59.604Z',
-    '2023-09-06T23:00:00.000Z',
-    '2023-09-09T23:36:17.929Z',
-    '2023-09-11T10:51:36.790Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -78,7 +78,7 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 // FUNCTIONS //
 
-const formatMovementDate = date => {
+const formatMovementDate = (date, locale) => {
   const calcDaysPassed = (date1, date2) =>
   Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
   const daysPassed = calcDaysPassed(new Date(), date);
@@ -87,10 +87,7 @@ const formatMovementDate = date => {
   if (daysPassed === 1) return 'Yesterday';
   if (daysPassed <= 7) return `${daysPassed} days ago`;
   else {
-    const day = `${date.getDate()}`.padStart(2, 0);
-    const month = `${date.getMonth() + 1}`.padStart(2, 0);
-    const year = date.getFullYear();
-    return `${day}/${month}/${year}`;
+    return new Intl.DateTimeFormat(locale).format(date);
   }
 
 };
@@ -105,7 +102,7 @@ const displayMovements = function (acc, sort = false) {
   movs.forEach((mov, i) => {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
     const date = new Date(acc.movementsDates[i]);
-    const displayDate = formatMovementDate(date);
+    const displayDate = formatMovementDate(date, acc.locale);
     const html = `
     <div class="movements__row">
       <div class="movements__type movements__type--${type}">
@@ -213,9 +210,9 @@ btnLogin.addEventListener('click', e => {
       hour: 'numeric',
       minute: 'numeric',
       day: 'numeric',
-      month: 'long',
+      month: 'numeric',
       year: 'numeric',
-      weekday: 'short'
+      // weekday: 'short'
     };
     labelDate.textContent = new Intl.DateTimeFormat(currentAccount.locale, options).format(now);
 
